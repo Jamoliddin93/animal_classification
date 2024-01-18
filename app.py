cdf8431594564a0cd10c1ae62382a2d2a8a21e7f
Jamoliddin Tangirberdiev <47080059+Jamoliddin93@users.noreply.github.com>
2024-01-18 21:23:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from fastai.vision.all import *
 from fastai.learner import CastToTensor
 import plotly.express as px
-import pathlib
+from pathlib import Path
 import platform
 
 plt = platform.system()
