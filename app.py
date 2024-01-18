5659d8e788651346dc6ecd8d0b6169d4bf073fce
Jamoliddin Tangirberdiev <47080059+Jamoliddin93@users.noreply.github.com>
2024-01-18 21:44:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import plotly.express as px
 import pathlib
 import platform
 
-temp = pathlib.PosixPath
-pathlib.PosixPath = pathlib.WindowsPath
+plt = platform.system()
+if plt=='Linux':pathlib.WindowsPath = pathlib.PosixPath
 
 st.title('Jonzotlarni klassifikatsiya qiluvchi model')
 st.text('Ushbu model 4 turdagi jonzotlarni o\'z ichiga oladi\n 1.Yirtqichlar 2.Hashorotlar  3.Reptilyalar 4. Qushlar')
